b90e4951b985cea5be336505f2bdacbab15fde19
Azim6917 <sarwadazim786@gmail.com>
2026-04-27 21:11:02 +0530

    Added Sentry monitoring

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import * as Sentry from "@sentry/react";
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+
+Sentry.init({
+  dsn: "https://03c513f1a827aaa78fa5c2f660308012@o4511292700688384.ingest.us.sentry.io/4511292719300608",
+  sendDefaultPii: false,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
